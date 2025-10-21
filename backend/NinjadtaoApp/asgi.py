17f0a2c0d0943339a995d5b8346ea2800952de94
David <Davidchanwsj@gmail.com>
2025-10-21 07:47:47 +0100

    Backend django project setup

---
+++ backend/NinjadtaoApp/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for NinjadtaoApp project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NinjadtaoApp.settings')
+
+application = get_asgi_application()
